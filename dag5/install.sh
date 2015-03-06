2d0de6b0d07229dbe56fdaf19e75c0f1d5bb236a
sindrevh <sindrevh@itk-d651>
2015-03-06 14:16:28 +0100

    Godkjent oving 8, sliter med å instalere driveer i terminal

--- dag5/install.sh
+++ dag5/install.sh
@@ -1,8 +1,10 @@
-#chmod +x ./install.sh
+#chmod a+x ./install.sh
+#chmod a+x $(pwd)install.sh
 
 #!/bin/bash
 export GOPATH=$(pwd)
 #mkdir $GOPATH
 go install driver
+echo "det kjorte"
 
 #go help gopath
