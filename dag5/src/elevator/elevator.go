eeb7bf9664ae975d207aa5d2e71c01de6f551d75
sindrevh <sindrevh@itk-d651>
2015-04-14 18:21:11 +0200

    Elevator ferdig?, no debug eller testet

--- dag5/src/elevator/elevator.go
+++ dag5/src/elevator/elevator.go
@@ -2,14 +2,14 @@ package elevator
 
 import (
 	"../driver"
+	"../init"
 	"../fmt"
 )
 
 const(
-	UP = 0
-	DOWN = 1
-	IDLE = 2
-	OPEN = 3
+	RUNUP = 1
+	STOPP = 0
+	RUNDOWN = -1
 
 	ON = 1
 	OFF = 0
@@ -18,12 +18,15 @@ const(
 
 var(
 	//for comunication with manager
+	Next_floor int
 	Next_floorCh = make(chan int)
+	Next_floor_doneCh = make (chan bool)
+	Current_floor int
 	Current_floorCh = make (chan int)
-	current_floor int
 	ElevDirectionCh = make (chan int)
-	ElevDirection int = DOWN
-	//Setter opp kananler
+	ElevDirection int = RUNDOWN
+
+	//Setter opp interne kananler /"states"
 	doorTimerStartCh = make(chan bool) //as bool
 	doorTimerStoppCh = make(chan bool) //as bool
 	idleCh = make(chan bool) //as bool
@@ -39,19 +42,20 @@ func Run(){
 	driver.Elev_init()
 	go DoorTimer()
 	go FloorLigths()
-	driver.ElevSetSpeed(-SPEED)
-	for driver.Elev_get_floor_sensor_signal() == -1{
+	driver.Elev_set_motor_direction(RUNDOWN)
+	for driver.Elev_get_floor_sensor_signal() == RUNDOWN{
 		time.Sleep(10*time.Millisecond)
 	}
-	driver.ElevSetSpeed(0)
+	driver.Elev_set_motor_direction(STOPP)
 	//Elevator initialized and in a definite floor
 	go Idle()
 	go Open()
 	go Down()
 	go Up()
+	go Range_safety()
+	go Update_channels()
 	idleCh <- true
 	<- done
-
 
 }
 
@@ -77,22 +81,18 @@ func FloorLigths(){
 func Idle(){
 	for{
 		<-idleCh
-		driver.Elev_set_motor_direction(0)
+		driver.Elev_set_motor_direction(STOPP)
 		for{
 			time.Sleep(10*time.Millisecond)
-			if Elev_get_floor_sensor_signal() == local_elevator_que[0]{// 0 indesksert
-				OpenDoorCh <- true
-				current_floor = Elev_get_floor_sensor_signal()
-				Current_floorCh <- current_floor//Det under kan evt gjøres via denne kanalen
-				//slett bestilling og flytt de andre elemntene ferem i lokal_que
-				//Må dette gjøres via pointer
+			if Elev_get_floor_sensor_signal() == Next_floor{
+				Open <- true
 				break
 			}
-			else if current_floor > local_elevator_que[0] {//neste etasje i ordre kø er under
+			else if current_floor > Next_floor {//neste etasje i ordre kø er under
 				elevDownCh <- true
 				break
 			}
-			else if current_floor < local_elevator_que[0]{//neste etasje i odrdre kø er over
+			else if current_floor < Next_floor {//neste etasje i odrdre kø er over
 				elevUpCh <- true
 				break
 			}
@@ -106,7 +106,10 @@ func Idle(){
 func Open(){
 	for{
 		<-openDoorCh
-		Elev_set_motor_direction(0)
+		Elev_set_motor_direction(ElevDirection *-1)
+		time.Sleep(50*time.Millisecond)
+		Elev_set_motor_direction(STOPP) 
+		//stop func evt
 		driver.Elev_set_door_open_lamp(ON)
 		doorTimerStartCh <- true
 		<-doorTimerStoppCh
@@ -114,21 +117,22 @@ func Open(){
 			time.Sleep(100*time.Millisecond)
 		}
 		driver.Elev_set_door_open_lamp(OFF)
+		Next_floor_doneCh <- true
 		idleCh <- true
 	}
 }
 
 func Up(){
 	<-elevUpCh
-	ElevDirection = UP
+	ElevDirection = RUNUP
 	driver.Elev_set_door_open_lamp(OFF)
-	Elev_set_motor_direction(1)
+	Elev_set_motor_direction(ElevDirection)
 	for{
 		for Elev_get_obstruction_signal == 1{
-			Elev_set_motor_direction(0)
+			Elev_set_motor_direction(STOPP)
 			time.Sleep(100*time.Millisecond)
 		}
-		if Elev_get_floor_sensor_signal() == local_elevator_que[0]{//Har nådd neste etasje i lokal heiskø
+		if Elev_get_floor_sensor_signal() == Next_floor{//Har nådd next_floor
 			OpenDoorCh <- true
 			break
 		}
@@ -138,19 +142,50 @@ func Up(){
 
 func Down(){
 	<-elevDownCh
-	ElevDirection = DOWN
+	ElevDirection = RUNDOWN
+	ElevDirectionCh <- ElevDirection
+	Elev_set_motor_direction(ElevDirection)
 	driver.Elev_set_door_open_lamp(OFF)
-	Elev_set_motor_direction(-1)
 	for{
 		for Elev_get_obstruction_signal == 1{
-			Elev_set_motor_direction(0)
+			Elev_set_motor_direction(STOPP)
 			time.Sleep(100*time.Millisecond)
 		}
-		if Elev_get_floor_sensor_signal() == local_elevator_que[0]{
+		if Elev_get_floor_sensor_signal() == Next_floor{//Har nådd next_floor
 			OpenDoorCh <- true
 			break
 		}
 		//Do we need any saftey feature to prevent the eleavtor crash into the roof
 	}
+}
 
+func Update_channels(){
+	for{
+		Next_floor <- Next_floorCh
+		if Elev_get_floor_sensor_signal() >= 0{
+			Current_floorCh <- Elev_get_floor_sensor_signal()
+			Current_floor = Elev_get_floor_sensor_signal()
+		}
+		time.Sleep(100*time.Millisecond)
+	}
+}
+
+func Range_safety(){
+	for{
+		if Elev_get_floor_sensor_signal() == N_FLOORS{
+			ElevDirection = RUNDOWN
+		}
+		else if Elev_get_floor_sensor_signal() = 0{
+			ElevDirection = RUNUP
+		}
+		time.Sleep(10*time.Millisecond)
+	}
+}
+
+func Door_safety(){
+	for{
+		if Elev_get_floor_sensor_signal == -1{
+			Elev_set_door_open_lamp(OFF)
+		}
+	}
 }
