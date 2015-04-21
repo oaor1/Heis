ec48a3cddbe8d2eef54e69b4532cc5a2e78f6c95
oaoren <ola_oren@dhcp-027210.wlan.ntnu.no>
2015-04-21 11:06:07 +0200

    her er cost osv

--- dag5/src/cost/cost.go
+++ dag5/src/cost/cost.go
@@ -3,6 +3,28 @@
 
 
 
-calculate_cost(System_data init.System_data, New_auction_data init.Auction_data) int{
-	return 5
+
+
+
+calculate_cost(System_data types.System_data, New_auction_data types.Auction_data) int{
+	cost := 0
+	up_orders := 0
+	dwn_orders := 0
+	overlap_orders := 0
+	/*
+	+ antall bestillinger i samme retning ^Y  * distandse * konst
+	+ antall bestillinger i motsatt retning ^X * distanse * konst
+	- overlappende bestillinger
+	*/
+	for i := 0; i < N_FLOORS; i++ {
+		if System_data.M_handle_q[0][i] == 1{
+			up_orders++
+		}
+		if System_data.M_handle_q[1][i] == 1{
+			dwn_orders++
+		}
+		if System_data.internal_elev_out[/*my number*/][New_auction_data.]
+		
+	}
+	return cost
 }
